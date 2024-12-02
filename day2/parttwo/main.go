677f2e0e401a8765b79b0c644ea8163875aa46e1
ArminIrannejad <armin.irannejad@gmail.com>
2024-12-02 21:25:51 +0100

    Second part was a bit better than first

---
+++ day2/parttwo/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	safeReports := 0
+
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+
+		levelsStr := strings.Fields(line)
+		levels := make([]int, len(levelsStr))
+		for i, s := range levelsStr {
+			n, err := strconv.Atoi(s)
+			if err != nil {
+				return
+			}
+			levels[i] = n
+		}
+
+		if ProblemDampener(levels) {
+			safeReports++
+		}
+	}
+
+	fmt.Println(safeReports)
+}
+
+func ProblemDampener(levels []int) bool {
+	if isSafe(levels) {
+		return true
+	}
+
+	for i := 0; i < len(levels); i++ {
+		newLevels := append([]int{}, levels[:i]...)
+		newLevels = append(newLevels, levels[i+1:]...)
+		if isSafe(newLevels) {
+			return true
+		}
+	}
+
+	return false 
+}
+
+func isSafe(levels []int) bool {
+	if len(levels) < 2 {
+		return true
+	}
+
+	direction := 0 
+	for i := 1; i < len(levels); i++ {
+		diff := levels[i] - levels[i-1]
+		if diff == 0 {
+			return false
+		}
+
+		absDiff := math.Abs(float64(diff))
+		if absDiff < 1 || absDiff > 3 {
+			return false
+		}
+
+		currentDirection := 1
+		if diff < 0 {
+			currentDirection = -1
+		}
+
+		if direction == 0 {
+			direction = currentDirection
+		} else if direction != currentDirection {
+			return false
+		}
+	}
+
+	return true
+}
+
